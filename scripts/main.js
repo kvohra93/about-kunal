6ddfacb1bf7f6e9f392c2a546037b4f1f7f23c39
Kunal <70285661+kvohra93@users.noreply.github.com>
2020-10-02 16:36:51 -0500

    final changes to fix qunit global error
    addEventListener would show as null for pages the buttons were not found on. So used an if statement to check if they were present

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,41 @@
 console.log('START')
 console.log('FUNCTION DECLARATION')
 
-function cal(x,y){
-    return x/(y*y);
+function cal(x, y) {
+    return x / (y * y);
 }
-document.querySelector ("#cal").addEventListener('click', () =>{
+var x = document.querySelector("#cal");
+if(x){
+x.addEventListener('click', () => {
     const s = document.querySelector('#guest').value
     const i = parseFloat(document.querySelector('#weight').value)
     console.log(`i=${i}`)
     const j = parseFloat(document.querySelector('#height').value)
-    const l = cal(i,j )
+    const l = cal(i, j)
     const m = l.toFixed(2)
     const ans = `${s} Your BMI is ${m}`
     document.querySelector('#result').innerHTML = ans
     console.log('Finished calculation of BMI')
+})
 document.querySelector('#clicker').addEventListener('click', () => {
     console.log('Starting clicker handler')
-    const s = document.querySelector('#guest').value
     const origCount = parseInt(localStorage.getItem('countOfClicks')) || 0
     const ct = origCount + 1
     const ans = `This button was pressed ${ct} times`
-    document.querySelector('#result').innerHTML = ans    
+    document.querySelector('#result').innerHTML = ans
     localStorage.setItem('countOfClicks', ct)
     console.log('  Finished clicker click handler')
-})
-})
-document.querySelector('#wiper').addEventListener('click', () => {
+})}
+y  = document.querySelector('#wiper')
+if (y){
+y.addEventListener('click', () => {
     console.log('  Starting wiper click handler')
     localStorage.removeItem('countOfClicks')
     console.log('  Finished wiper click handler - localStorage entries removed')
-})
+})}
 document.addEventListener('click', event => {
-    if (event.target && event.target.id === 'getRandomFact') { 
-      updateWithFact(event) 
+    if (event.target && event.target.id === 'getRandomFact') {
+        updateWithFact(event)
     }
-  })
+})
 console.log('script end')
